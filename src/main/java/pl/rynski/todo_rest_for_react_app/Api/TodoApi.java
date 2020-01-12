00e1d71b0d34129ea518beaa8739f567af3187b0
Michal Rynski <michalrynski96@gmail.com>
2020-01-13 00:17:58 +0100

    url changed

--- src/main/java/pl/rynski/todo_rest_for_react_app/Api/TodoApi.java
+++ src/main/java/pl/rynski/todo_rest_for_react_app/Api/TodoApi.java
@@ -10,7 +10,7 @@ import pl.rynski.todo_rest_for_react_app.service.TodoService;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://michu-react.herokuapp.com/")
+@CrossOrigin(origins = "http://michu-react.herokuapp.com/")
 public class TodoApi {
 
     private TodoService todoService;
